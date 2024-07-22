a9e4a69eddb2fd6209f38132f17d5d5ac8cc86f6
Ysmael Marques <ysmaelmarkss@gmail.com>
2024-07-22 19:22:36 -0300

    alterando carrossel

--- src/app/novos-cursos/page.tsx
+++ src/app/novos-cursos/page.tsx
@@ -1,19 +1,30 @@
 // NovosCursos.tsx
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import content from '../content/content.json';
 import CarouselNovoCurso from '../components/novosCursosComponents/CarouselNovoCurso';
 
 export default function NovosCursos() {
-  const { maintitle, courses } = content.novosCursos;
+  const { courses } = content.novosCursos;
 
   return (
     <div>
-      <Typography sx={{ marginTop: '30px' }} variant='h5' color='white'>
-        <strong>{maintitle}</strong>
-      </Typography>
-      <Box sx={{ marginTop: '20px' }}>
-        <CarouselNovoCurso courses={courses} />
+      <Box> {/* 1 wrapper */}
+        <Typography sx={{ marginTop: '30px' }} variant='h5' color='white'>
+          <strong>{content.novosCursos.maintitle}</strong>
+        </Typography>
+        <Box sx={{ marginTop: '20px' }}>
+          <CarouselNovoCurso courses={courses} itemsPerPage={3} />
+        </Box>
+      </Box>
+      <Box> {/* 2 wrapper*/}
+        <Typography sx={{ marginTop: '30px' }} variant='h5' color='white'>
+          <strong>{content.novosCursos.title}</strong>
+        </Typography>
+        <Button>Veja Mais</Button>
+        <Box>
+          <CarouselNovoCurso courses={courses} itemsPerPage={1} />
+        </Box>
       </Box>
     </div>
   );
