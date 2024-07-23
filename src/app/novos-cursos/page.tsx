f609b32ea1945df110cc779ee924f47df7408ff8
Ysmael Marques <90981943+ysmaelmarks@users.noreply.github.com>
2024-07-23 11:29:09 -0300

    consertando carrossel (#8)

--- src/app/novos-cursos/page.tsx
+++ src/app/novos-cursos/page.tsx
@@ -1,11 +1,12 @@
-// NovosCursos.tsx
 import React from 'react';
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 import content from '../content/content.json';
-import CarouselNovoCurso from '../components/novosCursosComponents/CarouselNovoCurso';
+import Carousel from '../components/novosCursosComponents/Carousel';
+import CardNovoCurso from '../components/novosCursosComponents/cardsNovosCursosDisponiveis';
+import CardCursosMaisComprados from '../components/novosCursosComponents/cardsCursosMaisComprados';
 
 export default function NovosCursos() {
-  const { courses } = content.novosCursos;
+  const { novosCursos, cursosMaisComprados } = content;
 
   return (
     <div>
@@ -14,16 +15,40 @@ export default function NovosCursos() {
           <strong>{content.novosCursos.maintitle}</strong>
         </Typography>
         <Box sx={{ marginTop: '20px' }}>
-          <CarouselNovoCurso courses={courses} itemsPerPage={3} />
+          <Carousel itemsPerPage={3} direction="column">
+            {novosCursos.courses.map((course, index) => (
+              <CardNovoCurso
+                key={index}
+                title={course.title}
+                subtitle={course.subtitle}
+                image={course.image}
+              />
+            ))}
+          </Carousel>
         </Box>
       </Box>
       <Box> {/* 2 wrapper*/}
-        <Typography sx={{ marginTop: '30px' }} variant='h5' color='white'>
-          <strong>{content.novosCursos.title}</strong>
-        </Typography>
-        <Button>Veja Mais</Button>
-        <Box>
-          <CarouselNovoCurso courses={courses} itemsPerPage={1} />
+        <Grid sx={{marginLeft:'-50px'}} container spacing={2} justifyContent="center">
+          <Grid item>
+            <Typography sx={{ marginTop: '30px' }} variant='h5' color='white'>
+              <strong>{content.cursosMaisComprados.maintitle}</strong>
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Button sx={{ color: '#fff', marginTop:'30px', textDecoration:'none' }}>
+              <Typography variant='subtitle2' sx={{textDecoration:'none'}}>Veja Mais</Typography>
+            </Button>
+          </Grid>
+        </Grid>
+        <Box sx={{marginBottom:'100px'}}>
+          <Carousel itemsPerPage={2} direction="row">
+            {cursosMaisComprados.courses.map((courses, index) => (
+              <CardCursosMaisComprados
+                key={index}
+                image={courses.image}
+              />
+            ))}
+          </Carousel>
         </Box>
       </Box>
     </div>
