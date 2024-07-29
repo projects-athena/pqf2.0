19ccf984613d6c56cf9acdcb8112ebca9071639b
Ysmael Marques <ysmaelmarkss@gmail.com>
2024-07-29 09:56:09 -0300

    adicionando layout inicial da page

--- src/app/components/Video/Video.tsx
+++ src/app/components/Video/Video.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 // eslint-disable-next-line react/display-name
 const Video = React.forwardRef<HTMLVideoElement, {}>((props, ref) => {
   return (
-    <video ref={ref} width="600" controls>
+    <video ref={ref} style={{ width: '100%' }} controls>
       <source src="/api/video" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -1,14 +1,44 @@
-mport React from 'react';
-import { Container, Typography } from '@mui/material';
+import React from 'react';
+import { Container, Typography, Box, Grid } from '@mui/material';
 import VideoPlayer from '../components/Video/VideoPlayer';
+import playerContent from '../content/player.content.json';
 
 const VideoPage: React.FC = () => {
+  const course = playerContent.playerCursos.cursos[0];
+
   return (
     <Container>
-      <Typography variant="h4" gutterBottom>
-        Video Player
-      </Typography>
-      <VideoPlayer />
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <Box sx={{ width: '100%', height: '100%', marginBottom: 2 }}>
+            <VideoPlayer />
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography sx={{ color: 'white' }} variant="h4" gutterBottom>
+            {course.nome}
+          </Typography>
+          <Typography sx={{ color: 'white' }} variant="body1" gutterBottom>
+            {course.descricao}
+          </Typography>
+          <Box sx={{ marginBottom: 2 }}>
+            <Typography sx={{ color: 'white' }} variant="h6">Rating: {course.rating}</Typography>
+          </Box>
+          <Box sx={{ marginBottom: 2 }}>
+            <Typography sx={{ color: 'white' }} variant="h6">Módulos do Curso</Typography>
+            <Box>
+              {course.aulas.map((aula) => (
+                <Box key={aula.id} sx={{ marginBottom: 1, padding: 1, cursor: 'pointer' }}>
+                  <Typography sx={{ color: 'white' }} variant="body1">{aula.nome}</Typography>
+                  <Typography sx={{ color: 'white' }} variant="body2">{aula.descricao}</Typography>
+                </Box>
+              ))}
+            </Box>
+          </Box>
+          <Typography sx={{ color: 'white' }} variant="h6" gutterBottom>Instrutor</Typography>
+          <Typography sx={{ color: 'white' }} variant="body1">{course.professor}</Typography>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
