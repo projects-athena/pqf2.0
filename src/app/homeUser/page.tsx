3c06f20d2a07d8ee055db69ec74aa8cd33f7fee4
erikmello589 <erikmello589@gmail.com>
2024-07-30 19:54:15 -0300

    Criando Card Destaque User

--- src/app/homeUser/page.tsx
+++ src/app/homeUser/page.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Button, Box, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import CursoDestaqueHomeUser from '../components/CursoDestaqueHomeUser/cursoDestaqueHomeUser';
+import content from '../content/userHome.content.json';
 
+const { maintitle, cursos } = content.cursosDestaqueHomeUser;
 
 const HomeUser: React.FC = () => {
   return (
-    
+    <Container maxWidth="lg">
+        <Box sx={{marginBottom: 4}}>
+            <Typography
+              variant="h1"
+              sx={{
+                color: 'white',
+                fontWeight: 'bold',
+                fontSize: '1.5rem',
+                paddingBottom: '5px',
+                px: '5%' ,
+                marginBottom: 1,
+              }}
+            >
+              {maintitle}
+          </Typography>
+
+          <CursoDestaqueHomeUser 
+          image={cursos[0].image}
+          professor={cursos[0].professor}
+          professorImage={cursos[0].professorImage}
+          curso={cursos[0].curso}
+          duracao={cursos[0].duracao}
+          avaliacao= {cursos[0].avaliacao}>
+          </CursoDestaqueHomeUser>
+        </Box>
+    </Container>
   );
 };
 
